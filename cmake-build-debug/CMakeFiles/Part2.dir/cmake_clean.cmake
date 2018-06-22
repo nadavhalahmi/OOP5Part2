a2baf84d7e657479ce4ec684ab5767299ab25019
nadav <nadavhalahmi@campus.technion.ac.il>
2018-06-22 13:42:16 +0300

    OOP5 initial

---
+++ cmake-build-debug/CMakeFiles/Part2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Part2.dir/main.cpp.obj"
+  "Part2.pdb"
+  "Part2.exe"
+  "Part2.exe.manifest"
+  "libPart2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Part2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Part2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Part2.
+# This may be replaced when dependencies are built.
